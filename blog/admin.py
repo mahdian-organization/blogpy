85116627f56b4aeb5327c56d437f24f5a0d520f4
ali mahdianrad <mahdian.organization@gmail.com>
2021-04-25 09:22:42 +0430

    add requirement file and custom admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import *
 
+# -------------------------------
+# adding models to admin panel
+"""
 admin.site.register(UserProfile)
 admin.site.register(Article)
-admin.site.register(Category)
+admin.site.register(Category)"""
+# -------------------------------
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'avatar', 'description']
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ['title', 'category', 'created_at']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cover']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -17,17 +17,18 @@ def validate_file_extension(value):
 
 class UserProfile(models.Model):
     # ایجاد رابطه یک به یک بین پروفایل کاربر و جدول User
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, verbose_name="نام کاربر", on_delete=models.CASCADE)
 
     # ایجاد یک تصویر برای کاربر
-    avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False,
+    avatar = models.FileField("تصویر", upload_to='files/user_avatar/', null=False, blank=False,
                               validators=[validate_file_extension])
 
     # توضیحات
-    description = models.CharField(max_length=512, null=False, blank=False)
+    description = models.CharField("توضیحات", max_length=512, null=False, blank=False)
 
-    def __str__(self):
-        return self.user.first_name + " " + self.user.last_name
+    #چون پنل ادمین سفارشی شده دیگر نیازی به override کردن متد زیر نیست
+    """def __str__(self):
+        return self.user.first_name + " " + self.user.last_name"""
 
 
 class Article(models.Model):
@@ -37,16 +38,17 @@ class Article(models.Model):
     content = RichTextField()
     created_at = models.DateTimeField(default=datetime.now)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+    # چون پنل ادمین سفارشی شده دیگر نیازی به override کردن متد زیر نیست
+    """def __str__(self):
+        return self.title"""
 
 
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/category_cover/', null=False, blank=False,
                              validators=[validate_file_extension])
-
-    def __str__(self):
-        return self.title
+    # چون پنل ادمین سفارشی شده دیگر نیازی به override کردن متد زیر نیست
+    """def __str__(self):
+        return self.title"""
